f878a761a241f871ea8f997ee2b4b7ed52e90b92
loukas <1216915972@qq.com>
2026-03-04 15:29:17 +0800

    06 add

---
+++ 06/mainwindow.h
@@ -0,0 +1,28 @@
+//
+// Created by zql on 2026/3/2.
+//
+
+
+#ifndef IQT_MAINWINDOW_H
+#define IQT_MAINWINDOW_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_mainwindow.h"
+class MainWindow:public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btnPlay_clicked();
+
+    void on_btnStop_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+
+#endif //IQT_MAINWINDOW_H
