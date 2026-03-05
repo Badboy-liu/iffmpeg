ef33c025aafdcf93934d65a8786fed7c4ae5dbfe
loukas <1216915972@qq.com>
2026-03-05 17:53:02 +0800

    07 add log

--- 07/main.cpp
+++ 07/main.cpp
@@ -1,7 +1,11 @@
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <iostream>
 #include <QApplication>
 #include <QDebug>
 
 #include "mainwindow.h"
+#include <windows.h>
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +13,12 @@ int main(int argc, char* argv[])
     MainWindow w;
     w.setWindowTitle(QObject::tr("Player"));
     w.show();
+    qInstallMessageHandler([](QtMsgType, const QMessageLogContext&, const QString& msg){
+        std::cout << msg.toStdString() << std::endl;
+    });
+
+    qDebug() << "qDebug test";
+    qInfo()  << "qInfo test";
     qDebug() << "Hello World";
     return QApplication::exec();
 }
